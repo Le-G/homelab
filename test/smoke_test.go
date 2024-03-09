daa0f01fe555602d74cea0823289efe0552a75f6
Le G <guillaume.legros.fr@gmail.com>
2024-03-09 14:02:45 +0100

    remove docker registry

--- test/smoke_test.go
+++ test/smoke_test.go
@@ -22,7 +22,6 @@ func TestSmoke(t *testing.T) {
 		{"gitea", "gitea"},
 		{"grafana", "grafana"},
 		{"homepage", "homepage"},
-		{"registry-docker-registry", "registry"},
 	}
 
 	for _, app := range mainApps {
